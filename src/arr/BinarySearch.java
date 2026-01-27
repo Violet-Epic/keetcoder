89c50ce0661a3bf085655f3eccc0f736ee5823d3
elian <xmc1781688798@163.com>
2026-01-27 21:43:06 +0800

    二分查找

---
+++ src/arr/BinarySearch.java
@@ -0,0 +1,22 @@
+package arr;
+
+public class BinarySearch {
+    /*给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+    ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1 */
+    public static int main(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                System.out.println(mid);
+                return mid;
+            } else if (nums[mid] > target) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
