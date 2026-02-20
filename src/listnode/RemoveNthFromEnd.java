916e056204a1c1dfa6f40ad620b78f23b145de55
Violet-Epic <xmc1781688798@163.com>
2026-02-20 12:34:19 +0800

    remove elements

---
+++ src/listnode/RemoveNthFromEnd.java
@@ -0,0 +1,17 @@
+package listnode;
+
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy, right = dummy;
+        while (n-- > 0) {
+            right = right.next;
+        }
+        while (right.next != null) {
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
